a5144f8f6b278be27667add9de93babbdbefa3a0
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2026-02-27 16:16:45 +0100

    Finish experimenting and prepare iterative OCR improvements

--- src/ocr-process.py
+++ src/ocr-process.py
@@ -8,7 +8,7 @@ from pandas import DataFrame
 
 if __name__ == '__main__':
     # Finds the name of all images in a folder
-    filePath = r'C:\Users\Rainybyte\Research\AllSky Light Pollution Project\2026-02-20\\'
+    filePath = r'C:\Users\Rainybyte\Research\AllSky Light Pollution Project\2019-09\\'
     allImages = os.listdir(filePath)
 
     # Important data lists
@@ -27,8 +27,8 @@ if __name__ == '__main__':
         image = Image.open(imagePath)
         images.append((file, image))
 
-    for (file, image) in images:
-        print(file)
+    # for (file, image) in images:
+    #     print(file)
 
         full_height = 480
         block_width = 80
@@ -43,17 +43,19 @@ if __name__ == '__main__':
         stat_block = Image.new('RGB', (block_width, top_height + bottom_height))
         stat_block.paste(top_block, (0, 0))
         stat_block.paste(bottom_block, (0, top_height))
-        stat_block = stat_block.resize((2 * block_width, 2 * (top_height + bottom_height)), Resampling.LANCZOS)
+        stat_block = stat_block.resize((3 * block_width, 2 * (top_height + bottom_height)), Resampling.LANCZOS)
 
         # Directs tesseract to the library
         pytesseract.tesseract_cmd = tesseractPath
 
+        test = pytesseract.image_to_data(stat_block, lang='eng+math', config='--psm 12 --oem 1')
+
         # Extracts the text
-        newData = pytesseract.image_to_string(stat_block)
-        newData = newData.splitlines()
+        newData = pytesseract.image_to_string(stat_block).splitlines()
+
 
         if len(newData) != 5:
-            print(f'Unexpected number of text lines in image: {file}')
+            print(f'Unexpected number of text lines in image: {file}, {len(newData)} instead of 5.')
             continue
 
         date.append(newData[0])
