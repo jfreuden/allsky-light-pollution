cc20928f88f9e707af6bb6b0a8e82641260cd863
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2026-04-01 16:39:47 -0400

    Allow other columns to linregress. This will be deprecated soon in favor of more usual methods

--- src/allsky/analysis.py
+++ src/allsky/analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import dask.dataframe as dd
-def quick_exposure_linregress(dataframe: pd.DataFrame | dd.DataFrame) -> tuple[float, float]:
+def quick_exposure_linregress(dataframe: pd.DataFrame | dd.DataFrame, y_variable="exposure") -> tuple[float, float]:
     x = dataframe["timestamp"].map(pd.Timestamp.toordinal).to_numpy()
-    y = dataframe["exposure"].to_numpy()
+    y = dataframe[y_variable].to_numpy()
     return np.polyfit(x, y, 1)
